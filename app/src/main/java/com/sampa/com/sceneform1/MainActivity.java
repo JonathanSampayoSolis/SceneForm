4312aac75b78f05c1994e838e9dd5f100c5d8b58
jjsampayo <jj.sampayo@mavi.mx>
2019-07-10 18:58:31 -0500

    Gesture already finished

--- app/src/main/java/com/sampa/com/sceneform1/MainActivity.java
+++ app/src/main/java/com/sampa/com/sceneform1/MainActivity.java
@@ -1,33 +1,49 @@
 package com.sampa.com.sceneform1;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.ar.core.exceptions.CameraNotAvailableException;
-import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.SceneView;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.FootprintSelectionVisualizer;
+import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.ar.sceneform.ux.TransformationSystem;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class MainActivity extends AppCompatActivity {
+	private static final String TAG = "ModelRenderingActivity";
 	
-	SceneView sceneView;
+	private SceneView sceneView;
 	
-	private Scene scene;
+	private boolean modelPlaced = false;
+	
+	private TransformationSystem transformationSystem;
+	private ModelRenderable mRender;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		loadModel("Chair.sfb");
 		setContentView(R.layout.activity_main);
-		
 		sceneView = findViewById(R.id.scene_view);
-		scene = sceneView.getScene();
 		
-		renderObject("Chair.sfb");
+		transformationSystem = new TransformationSystem(getResources().getDisplayMetrics(), new FootprintSelectionVisualizer());
+		sceneView.getScene().addOnUpdateListener(this::onFrameUpdate);
+		sceneView.getScene().addOnPeekTouchListener((hitTestResult, motionEvent) -> transformationSystem.onTouch(hitTestResult, motionEvent));
+		
+		sceneView.getScene().getCamera().setLocalPosition(new Vector3(0, 0.2f, 0));
 	}
 	
 	@Override
@@ -47,35 +63,45 @@ public class MainActivity extends AppCompatActivity {
 	}
 	
 	
-	// region:: PRIVATE METHODS
-	
-	private void renderObject(@SuppressWarnings("SameParameterValue") String sfb) {
-		ModelRenderable.builder()
-				.setSource(this, Uri.parse(sfb))
-				.build()
-				.thenAccept(this::addNodeToScene)
-				.exceptionally(throwable -> {
-					Log.e("MainActivity", throwable.getMessage());
-					throwable.printStackTrace();
-					return null;
-				});
+	private void onFrameUpdate(FrameTime frameTime) {
+		Log.d(TAG, "onFrameUpdate");
+		if (!modelPlaced) {
+			placeModel();
+		}
 	}
 	
-	// endregion
 	
-	// region:: REFERENCE METHODS
-	
-	private void addNodeToScene(ModelRenderable render) {
-		Node node = new Node();
+	private void placeModel() {
+		if (mRender == null) {
+			Log.d(TAG, "Renderable not yet ready");
+			return;
+		}
 		
-		node.setParent(scene);
-		node.setLocalPosition(new Vector3(0f, -1f, -2f));
-		node.setName("Chair");
-		node.setRenderable(render);
-		
-		scene.addChild(node);
+		//Node modelNode = new Node();
+		TransformableNode modelNode = new TransformableNode(transformationSystem);
+		modelNode.getRotationController().setEnabled(true);
+		modelNode.getScaleController().setEnabled(true);
+		modelNode.getTranslationController().setEnabled(false);
+		modelNode.setRenderable(mRender);
+		sceneView.getScene().addChild(modelNode);
+		modelNode.setLocalPosition(new Vector3(0, -1f, -2f));
+		Log.d(TAG, "Placed renderable");
+		modelPlaced = true;
 	}
 	
-	// endregion
 	
+	private void loadModel(String model) {
+		CompletableFuture<ModelRenderable> future =
+				ModelRenderable.builder()
+						.setSource(this, Uri.parse(model))
+						.build()
+						.thenApply(renderable -> mRender = renderable)
+						.exceptionally(throwable -> this.onException(model, throwable));
+	}
+	
+	
+	ModelRenderable onException(String id, Throwable throwable) {
+		Log.d(TAG, "Unable to load renderable: " + id, throwable);
+		return null;
+	}
 }
