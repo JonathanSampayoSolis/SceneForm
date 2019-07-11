64a140f461d0c29ad63b6ed7aa671e7c29b4cf2b
jjsampayo <jj.sampayo@mavi.mx>
2019-07-11 16:52:10 -0500

    Controllers crafted

--- app/src/main/java/com/sampa/com/sceneform1/MainActivity.java
+++ app/src/main/java/com/sampa/com/sceneform1/MainActivity.java
@@ -3,8 +3,12 @@ package com.sampa.com.sceneform1;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.sceneform.FrameTime;
@@ -14,6 +18,8 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.FootprintSelectionVisualizer;
 import com.google.ar.sceneform.ux.TransformableNode;
 import com.google.ar.sceneform.ux.TransformationSystem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -30,15 +36,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		sceneView = findViewById(R.id.scene_view);
 		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		
+		setSupportActionBar(toolbar);
+		Objects.requireNonNull(getSupportActionBar()).setTitle("SKU12345");
+		
 		loadModel("Chair.sfb");
-		
-		transformationSystem = new TransformationSystem(getResources().getDisplayMetrics(), new FootprintSelectionVisualizer());
-		sceneView.getScene().addOnUpdateListener(this::onFrameUpdate);
-		sceneView.getScene().addOnPeekTouchListener(transformationSystem::onTouch);
-		
-		sceneView.getScene().getCamera().setLocalPosition(new Vector3(0, 0.2f, 0));
 	}
 	
 	@Override
@@ -57,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
 		sceneView.pause();
 	}
 	
-	
-	private void onFrameUpdate(@SuppressWarnings("unused") FrameTime frameTime) {
-		if (!modelPlaced) {
-			renderModel();
-		}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
 	}
 	
+	// region:: PRIVATE METHODS
 	
 	private void renderModel() {
 		if (mRender == null) {
@@ -89,11 +95,34 @@ public class MainActivity extends AppCompatActivity {
 				.build()
 				.thenApply(renderable -> mRender = renderable)
 				.exceptionally(throwable -> this.onException(model, throwable));
+		
+		applyGesture();
+	}
+	
+	private void applyGesture() {
+		transformationSystem = new TransformationSystem(getResources().getDisplayMetrics(), new FootprintSelectionVisualizer());
+		sceneView.getScene().addOnUpdateListener(this::onFrameUpdate);
+		sceneView.getScene().addOnPeekTouchListener(transformationSystem::onTouch);
+		
+		sceneView.getScene().getCamera().setLocalPosition(new Vector3(0, 0.2f, 0));
 	}
 	
 	ModelRenderable onException(String id, Throwable throwable) {
 		Log.d(TAG, "Unable to load renderable: " + id, throwable);
 		return null;
 	}
+	
+	// endregion
+	
+	// region:: REFERENCE METHODS
+	
+	private void onFrameUpdate(@SuppressWarnings("unused") FrameTime frameTime) {
+		if (!modelPlaced) {
+			renderModel();
+		}
+	}
+	
+	// endregion
+	
 	
 }
